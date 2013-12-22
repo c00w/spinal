9db065370731e7f1559112d261b1ac822a1d7752
Tristan Rice <rice@outerearth.net>
2013-12-22 02:21:24 -0800

    Arbitrary sized k values

--- overall_test.go
+++ overall_test.go
@@ -18,7 +18,26 @@ func TestInOut(t *testing.T) {
     if len(resp) !=len(input) {
         t.Fatal("Length is incorrect")
     }
-    
+
+    if string(resp) != string(input) {
+        t.Fatal(string(resp), "  !=  ", string(input))
+    }
+
+}
+func TestInOutBigK(t *testing.T) {
+    h := crc32.NewIEEE()
+    k := 2
+    d := 1
+    B := 4
+    input := []byte("Hello world! My name is betty jone")
+    out, _ := Encode(k, h, input, len(input))
+    resp := Decode(len(input), k, d, B, h, out)
+
+
+    if len(resp) !=len(input) {
+        t.Fatal("Length is incorrect")
+    }
+
     if string(resp) != string(input) {
         t.Fatal(string(resp), "  !=  ", string(input))
     }
