d1165c005e77d7140ebf75f640af8af45e97814e
Colin Rice <colin@daedrum.net>
2013-12-22 16:24:07 -0800

    Slightly more robust test suite

--- overall_test.go
+++ overall_test.go
@@ -6,21 +6,26 @@ import (
 )
 
 func TestInOut(t *testing.T) {
-	h := crc32.NewIEEE()
-	k := 1
-	d := 1
-	B := 4
-	input := []byte("Hello world! My name is betty jone")
-	out, _ := Encode(k, h, input, len(input)*2)
-	resp := Decode(len(input), k, d, B, h, out)
+    testcases := [][]byte{
+        []byte("foo"),
+        []byte("Hello World!"),
+        []byte("Hello World! My name is betty jones")}
+    for _, input := range testcases {
+        h := crc32.NewIEEE()
+        k := 1
+        d := 1
+        B := 32
+        out, _ := Encode(k, h, input, len(input)*2)
+        resp := Decode(len(input), k, d, B, h, out)
 
-	if len(resp) != len(input) {
-		t.Fatal("Length is incorrect")
-	}
+        if len(resp) != len(input) {
+            t.Fatal("Length is incorrect")
+        }
 
-	if string(resp) != string(input) {
-		t.Fatal(string(resp), "  !=  ", string(input))
-	}
+        if string(resp) != string(input) {
+            t.Fatal(string(resp), "  !=  ", string(input))
+        }
+    }
 
 }
 func TestInOutBigK(t *testing.T) {
